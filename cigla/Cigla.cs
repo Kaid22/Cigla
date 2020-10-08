f024062c0272800d30afeb97f547a0735af48da2
Kaid22 <avdickaid@gmail.com>
2020-10-08 14:00:25 +0200

    commit

---
+++ cigla/Cigla.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace cigla
+{
+    class Cigla
+    {
+        Point o;
+        int a, b;
+        public void Postavi(int a, int b, Point o)
+        {
+            if(a>=0 && b >= 0)
+            {
+                this.a = a;
+                this.b = b;
+                this.o = o;
+            }
+            else
+            {
+                throw new Exception("BROJ JE NEGATIVAN");
+            }
+        }
+        public void Postavi(int a, int b, int x, int y)
+        {
+            if(a>=0 && b >= 0)
+            {
+                this.a = a;
+                this.b = b;
+                this.o = new Point(x, y);
+            }
+            else
+            {
+                throw new Exception("BROJ JE NEGATIVAN");
+            }
+        }
+        public void Crtaj(Graphics g)
+        {
+            Pen olovka = new Pen(Color.Black);
+            SolidBrush cetka = new SolidBrush(Color.RosyBrown);
+            g.FillRectangle(cetka, o.X - a / 2, o.Y - b / 2, a, b);
+            g.DrawRectangle(olovka, o.X - a / 2, o.Y - b / 2, a, b);
+        }
+    }
+}
--- cigla/Form1.cs
+++ cigla/Form1.cs
@@ -10,11 +10,45 @@ using System.Windows.Forms;
 
 namespace cigla
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int a = 80, b = 40, x, y;
+        int red = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point o = new Point(x, y);
+            Graphics g = CreateGraphics();
+            Cigla c = new Cigla();
+            c.Postavi(a, b, o);
+            c.Crtaj(g);
+            x += a;
+            if (x > ClientRectangle.Width)
+            {
+                if (red % 2 == 0)
+                {
+                    x = 0;
+                    y -= b;
+                }
+                else
+                {
+                    x = a / 2;
+                    y -= b;
+                }
+                red++;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            x = a / 2;
+            y = ClientRectangle.Height - b / 2;
         }
     }
 }
